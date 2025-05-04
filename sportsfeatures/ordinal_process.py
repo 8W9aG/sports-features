1450517e74ef4a3166286ae2e6bd4e128de34f92
Will Sackfield <will.sackfield@gmail.com>
2025-05-04 09:45:18 -0400

    Fix categorical features

--- sportsfeatures/ordinal_process.py
+++ sportsfeatures/ordinal_process.py
@@ -8,13 +8,19 @@ def ordinal_process(df: pd.DataFrame, categorical_features: set[str]) -> pd.Data
     """Process ordinal features."""
     if not categorical_features:
         return df
+    true_categorical_features: list[str] = []
+    for categorical_feature in categorical_features:
+        if categorical_feature not in df.columns.values:
+            continue
+        true_categorical_features.append(categorical_feature)
+        df[categorical_feature] = df[categorical_feature].astype("category")
     od = OrdinalEncoder(
         missing_values="ignore",
         encoding_method="arbitrary",
-        variables=list(categorical_features),
+        variables=true_categorical_features,  # type: ignore
     )
     df = od.fit_transform(df)
-    for categorical_feature in categorical_features:
+    for categorical_feature in true_categorical_features:
         if categorical_feature not in df.columns.values:
             continue
         try:
